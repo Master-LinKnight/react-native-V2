405287b8f4c5ba0ffa86d276fb1f3f9e3ed6ce4a
LinKnight <Knight.Lin@mkcorp.com>
2017-11-08 15:02:00 +0800

    test

---
+++ app.js
@@ -0,0 +1,40 @@
+//app.js
+/**  作者：Code4Android
+ *   项目地址：https://github.com/xiehui999/SmallAppForQQ
+ *   新浪微博：http://weibo.com/745687294
+ *   CSDN  :    http://blog.csdn.net/xiehuimx?viewmode=contents
+ *   简书   :   http://www.jianshu.com/users/d5b531888b2b/latest_articles
+ * */
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+  getUserInfo:function(cb){
+    var that = this
+    this.globalData.user="1111"
+    console.log(this.globalData.user1)
+    if(this.globalData.userInfo){
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    }else{
+      //调用登录接口
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
+  },
+  globalData:{
+    userInfo:null,
+    user:'',
+    user1:"222"
+  }
+})
