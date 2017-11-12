38bc605d057db153e589292b87ace03fd2c77756
LinKnight <Knight.Lin@mkcorp.com>
2017-11-12 12:54:13 +0800

    submit by knight
    submit by knight -- add 0.44.0 project

---
+++ myGalaxy/android/app/src/main/java/com/mygalaxy/MainActivity.java
@@ -0,0 +1,24 @@
+package com.mygalaxy;
+
+import com.facebook.react.ReactActivity;
+import android.os.Bundle;
+import com.cboy.rn.splashscreen.SplashScreen;
+import com.cboy.rn.splashscreen.SplashScreenReactPackage;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "myGalaxy";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // 添加这一句
+        super.onCreate(savedInstanceState);
+    }
+}
