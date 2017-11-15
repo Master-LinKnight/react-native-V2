86ba87cdb139cd3f40793725ae863d3bb85188b3
LinKnight <Knight.Lin@mkcorp.com>
2017-11-15 15:03:12 +0800

    add 2 new page
    movie and topic

--- myGalaxy/app/pages/index/index.js
+++ myGalaxy/app/pages/index/index.js
@@ -13,6 +13,9 @@ import {
     View,
     TouchableWithoutFeedback,
 } from 'react-native';
+import { StackNavigator,TabNavigator,DrawerNavigator } from 'react-navigation';
+import Topic from '../topic/topic'
+import Movie from '../movie/movie'
 
 export default class Index extends Component {
     constructor(props){
@@ -24,7 +27,7 @@ export default class Index extends Component {
             title:'Playground',
             headerTitleStyle: {
                 color: '#ffffff',
-                fontSize: 32,
+                fontSize: 36,
                 textAlign: 'center',
                 marginTop: 16,
             },
@@ -40,11 +43,54 @@ export default class Index extends Component {
     }
 
     render() {
-        // Navigation = this.props.navigation;
+        const TabScreen = TabNavigator({
+                Topic: {
+                    screen: Topic,
+                    navigationOptions: {
+                        tabBarLabel: '话题广场'
+                    }
+                },
+                Movie: {
+                    screen: Movie,
+                    navigationOptions: {
+                        tabBarLabel: '电影院线'
+                    }
+                }
+            },
+            {
+                animationEnabled: true,
+                lazy: true,
+                swipeEnabled: false,
+                tabBarPosition: 'bottom',
+                tabBarOptions: {
+                    activeTintColor: '#3b5597',
+                    // activeBackgroundColor: '#3b5597',
+                    inactiveTintColor: '#8a8a8a',
+                    showIcon: true,
+                    indicatorStyle: {
+                        opacity: 0
+                    },
+                    tabStyle: {
+                        padding: 0,
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                    },
+                    labelStyle: {
+                        marginBottom: 10,
+                        fontSize: 24 // 文字大小
+                    },
+                    style: {
+                        // backgroundColor: 'rgba(255,255,255,0.1)', // TabBar 背景色
+                        backgroundColor: '#ffffff',
+                        height: 100
+                    }
+                },
+                backBehavior: 'none',
+            });
 
         return (
             <View style={styles.container}>
-
+                <TabScreen/>
             </View>
         );
     }
---
+++ myGalaxy/app/pages/topic/topic.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    Platform,
+    Image,
+    StyleSheet,
+    ScrollView,
+    View,
+    TouchableWithoutFeedback,
+} from 'react-native';
+
+export default class Topic extends Component {
+    constructor(props){
+        super(props);
+
+    }
+
+    render() {
+        // Navigation = this.props.navigation;
+
+        return (
+            <View style={styles.container}>
+
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#efefef'
+    }
+});
+
+
+
